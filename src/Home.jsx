df24e784812c85d5c05fea46c20d97204abc8ec1
elroi773 <s2457@e-mirim.hs.kr>
2025-11-02 23:43:14 +0900

    css 수정 , 라우터 문제 해결

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import './Home.css'
+import './Home.css';
 
-function Home() {
+export default function Home() {
   return (
-    <>
-      <div>
-          <h1>사람이 되자</h1>
-          <p>거북이에서 사람으로,</p>
-          <button>start</button>
-      </div>
-    </>
-  )
-}
+    <div className="wrap">
+      {/* 빛나는 동그라미는 장식용 요소로 분리 */}
+      <div className="orb" aria-hidden="true" />
 
-export default App
+      <h1>사람이 되자</h1>
+      <p>거북이에서 사람으로,</p>
+      <button className="cta">start</button>
+    </div>
+  );
+}
