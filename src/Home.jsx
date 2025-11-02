e77d1092d40f6f964921684546e771bbdc2c71e5
elroi773 <s2457@e-mirim.hs.kr>
2025-11-02 23:33:29 +0900

    라우터 추가 및 index animation, home화면 구현

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <div>
+          <h1>사람이 되자</h1>
+          <p>거북이에서 사람으로,</p>
+          <button>start</button>
+      </div>
+    </>
+  )
+}
+
+export default App
