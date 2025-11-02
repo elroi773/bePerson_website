62db40cdd95680ee3f9ec902f49fa8b5da599405
elroi773 <s2457@e-mirim.hs.kr>
2025-11-02 23:33:24 +0900

    라우터 추가 및 index animation, home화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
 
 function App() {
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
