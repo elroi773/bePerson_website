4bfbbb77409b54e117ed9eb07804449cee6a547d
elroi773 <s2457@e-mirim.hs.kr>
2025-11-03 22:52:35 +0900

    index 화면 구현 중 ...

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,5 +1,9 @@
 // Index.jsx
 export default function Index() {
-    return <div>Index Page</div>;
-  }
+    return (
+      <div>
+         <img src="https://capsule-render.vercel.app/api?type=waving&color=4b6f49&height=200&width=2000&animation=blinking&fontColor=fffdc7&fontSize=60" />
+      </div>
+    );
+}
   
