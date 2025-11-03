67930bec7bd319312baa57ad5bb86778ce0b0ea4
elroi773 <s2457@e-mirim.hs.kr>
2025-11-03 22:52:32 +0900

    index 화면 구현 중 ...

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 import './Home.css';
+import { useNavigate } from "react-router-dom";
+
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="wrap">
       {/* 빛나는 동그라미는 장식용 요소로 분리 */}
@@ -8,7 +12,7 @@ export default function Home() {
 
       <h1>사람이 되자</h1>
       <p>거북이에서 사람으로,</p>
-      <button className="cta">start</button>
+      <button className="cta" onClick={() => navigate("/index")}>start</button>
     </div>
   );
 }
